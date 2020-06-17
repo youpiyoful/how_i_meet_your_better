e26215c576de7ec027f257f9c65342f4c979cbdc
youpiyoful <yoanfornari@gmail.com>
2020-06-17 03:26:47 +0200

    [add] add debug toolbar

--- himyb/settings.py
+++ himyb/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'himyb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'himyb',
+        'USER': 'youpiyoful',
+        'PASSWORD': 12345,
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
@@ -118,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# INTERNAL_IPS = [
+#     '127.0.0.1',
+# ]
