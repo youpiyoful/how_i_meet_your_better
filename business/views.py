d0ea2c357e1c03d4142f6383827058e7ca4a024f
youpiyoful <yoanfornari@gmail.com>
2020-06-25 05:24:41 +0200

    [init] a simple hello world for test our view

--- business/views.py
+++ business/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    """return the home template of our app"""
+    return HttpResponse("Hello, world !")
+
