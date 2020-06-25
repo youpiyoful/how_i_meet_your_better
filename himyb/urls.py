641c65683d4653c2defd071e4e75d4b87ef77477
youpiyoful <yoanfornari@gmail.com>
2020-06-25 05:31:48 +0200

    [add] i add the connection with business.urls

--- himyb/urls.py
+++ himyb/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('business', include('business.urls')),
     path('admin/', admin.site.urls),
-    # path('/', home),
 ]
 
 if settings.DEBUG:
