d4cf4f9a200ad1bf670420be85a52479ec111929
youpiyoful <yoanfornari@gmail.com>
2020-06-26 04:37:51 +0200

    [change] i moove '/' in the main urls.py

--- business/urls.py
+++ business/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='index')
+    # path('', views.index, name='index'),
+    path('<name>/display_name', views.display_name, name='display_name')
 ]
