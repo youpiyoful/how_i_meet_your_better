d0a4cbc625841827d3f70002731fff97f77c4e1b
youpiyoful <yoanfornari@gmail.com>
2020-06-27 06:22:54 +0200

    [add] add the results url

--- business/urls.py
+++ business/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('<name>/display_name', views.display_name, name='display_name')
+    path('<name>/display_name', views.display_name, name='display_name'),
+    path('results', views.results, name='results')
 ]
