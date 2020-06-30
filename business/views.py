b1c510378a46b1c0e7eb2e60f04add376873d309
youpiyoful <yoanfornari@gmail.com>
2020-07-01 01:38:00 +0200

    [rollback] finaly delete the variable active_home for index

--- business/views.py
+++ business/views.py
@@ -9,7 +9,7 @@ def index(request):
     # return HttpResponse("Hello world, look at this <a href='http://127.0.0.1:8000/himyb/Yoan%20Fornari/display_name'>link</a>")
     # template = loader.get_template('business/index.html')
     # context = {"coucou": "coucou"}
-    return render(request, 'business/index.html', {'active_home': 'active'})
+    return render(request, 'business/index.html')
     # return HttpResponse(template.render(request))
 
 def results(request):
