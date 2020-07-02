fa577347ad6d11b08fe50cee11eed24024d9fbae
youpiyoful <yoanfornari@gmail.com>
2020-07-03 01:43:33 +0200

    [add] add user to the dir of path template staticfiles

--- himyb/settings.py
+++ himyb/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -58,6 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'business/templates'),
+            os.path.join(BASE_DIR, 'user/templates'),
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "business/static"),
+    os.path.join(BASE_DIR, "user/static")
 ]
 
 # Static files (CSS, JavaScript, Images)
