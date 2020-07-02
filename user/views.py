23a7b2941205d405af9060a48bc63e4a1f063cc1
youpiyoful <yoanfornari@gmail.com>
2020-07-03 01:43:55 +0200

    [fix] use return for render the template

--- user/views.py
+++ user/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def my_account(request):
     """render the account of user"""
-    render(request, 'user/account.html')
+    return render(request, 'user/account.html')
