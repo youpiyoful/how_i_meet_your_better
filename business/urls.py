a7b399dde855320f4b5221e1810839f24713db4e
youpiyoful <yoanfornari@gmail.com>
2020-07-03 17:02:56 +0200

    [create] create a new url for detail-food page

--- business/urls.py
+++ business/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('<name>/display_name', views.display_name, name='display_name'),
-    path('results', views.results, name='results')
+    path('results', views.results, name='results'),
+    path('<food>/detail-food', views.detail_food, name='detail_food'),
 ]
