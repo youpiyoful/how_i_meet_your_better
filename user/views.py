95650eef829f7f95eb28a25facdf805d9260a56a
youpiyoful <yoanfornari@gmail.com>
2020-07-06 16:44:01 +0200

    [create] create a set of data for front design

--- user/views.py
+++ user/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def my_account(request):
     """render the account of user"""
-    return render(request, 'user/account.html')
+    context = {
+        'user':
+            {
+                'name': 'Yoan Fornari',
+                'email': 'yoanfornari@gmail.com',
+                'age': '28',
+                'adresse': '45 cours du parc 21 000, Dijon'
+            }
+    }
+    return render(request, 'user/account.html', context)
