fb800ce6a86dc46d95e6a74a187eabc15119db41
youpiyoful <yoanfornari@gmail.com>
2020-07-07 15:48:14 +0200

    :construction: add new url
    add url register and login and modify login  and register in render-login-page and render-register-page

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('account', views.my_account, name='my_account'),
+    path('login-page', views.render_login_page, name='render_login_page'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('register', views.register, name='register')
+    path('register-page', views.render_register_page, name='render_register_page'),
+    path('regiter', views.register, name='register')
 ]
