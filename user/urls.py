c2c43034ccb79649e0b2f6406181cd0c774ba81e
youpiyoful <yoanfornari@gmail.com>
2020-07-07 14:44:06 +0200

    :construction: add urls in urls.py of users application
    i add url for login, logout, and register

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('account', views.my_account, name='my_account'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('register', views.register, name='register')
 ]
