25fd98e65f99adcec96f5a96164197998edf86c3
youpiyoful <yoanfornari@gmail.com>
2020-07-07 15:56:35 +0200

    :sparkles: create new fonctions in view
    I created the seen functions corresponding to the urls / endpoint created in urls.py

--- user/views.py
+++ user/views.py
@@ -5,12 +5,32 @@ from django.shortcuts import render
 def my_account(request):
     """render the account of user"""
     context = {
-        'user':
-            {
-                'name': 'Yoan Fornari',
-                'email': 'yoanfornari@gmail.com',
-                'age': '28',
-                'adresse': '45 cours du parc 21 000, Dijon'
-            }
+        "user": {
+            "name": "Yoan Fornari",
+            "email": "yoanfornari@gmail.com",
+            "age": "28",
+            "adresse": "45 cours du parc 21 000, Dijon",
+        }
     }
-    return render(request, 'user/account.html', context)
+    return render(request, "user/account.html", context)
+
+
+def render_login_page(request):
+    """return the render page for login"""
+    return render(request, "user/login.html")
+
+
+def logout(request):
+    """call the metodh logout and redirect on home page"""
+    return render(request, "business/index.html")
+
+
+def render_register_page(request):
+    """return the register page"""
+    return render(request, "user/register.html")
+
+
+def register(request):
+    """record a new user"""
+    # render(request, 'coucou')
+    pass
