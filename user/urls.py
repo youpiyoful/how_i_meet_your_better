715d99991bdf52cd3d87d269906a8faca2730f40
youpiyoful <yoanfornari@gmail.com>
2020-07-07 15:51:53 +0200

    :art: modify indetation
    change indentation for pep8 confluence

--- user/urls.py
+++ user/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account', views.my_account, name='my_account'),
-    path('login-page', views.render_login_page, name='render_login_page'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('register-page', views.render_register_page, name='render_register_page'),
-    path('regiter', views.register, name='register')
+    path("account", views.my_account, name="my_account"),
+    path("login-page", views.render_login_page, name="render_login_page"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path(
+            "register-page",
+            views.render_register_page,
+            name="render_register_page"
+        ),
+    path("regiter", views.register, name="register"),
 ]
