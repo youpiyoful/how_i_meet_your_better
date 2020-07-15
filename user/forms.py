81df14cd53702cace48a6e3185c1cb95f3ee87d8
youpiyoful <yoanfornari@gmail.com>
2020-07-15 03:17:41 +0200

    :construction: add fields of form in attribute
    add firstname and lastname field attribute object

--- user/forms.py
+++ user/forms.py
@@ -2,5 +2,29 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    firstname = forms.CharField(label='firstname')
+    firstname = forms.CharField(
+        # label="prénom",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'firstname',
+                'aria-describedby': 'inputFirstname'
+            }
+        )
+    )
+    lastname = forms.CharField(
+        # label="nom",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'lastname',
+                'aria-describedby': 'inputLastname'
+            }
+        )
+    )
+    email = forms.EmailField()
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput())
 
+# TODO faire le form pour la connexion
+
+# class LoginForm
