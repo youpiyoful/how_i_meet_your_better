68e61ab6f8018451e5f7b7eb44a540dbc89599d0
youpiyoful <yoanfornari@gmail.com>
2020-07-17 19:20:55 +0200

    change name of login and logout function

--- user/urls.py
+++ user/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path("account", views.my_account, name="my_account"),
-    path("login", views.login, name="login"),
+    path("login", views.authentication, name="login"),
     # path("loger", views.loger, name="loger"),
-    path("logout", views.logout, name="logout"),
+    path("logout", views.logout_view, name="logout"),
     path(
             "register",
             views.register,
