8e80295cd3f350c59e1e23d156dcdd6df96201d7
youpiyoful <yoanfornari@gmail.com>
2020-07-19 05:16:04 +0200

    extend user model with purebeurreuser class

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
+"""
+extention of user model
+"""
 from django.db import models
+from django.contrib.auth.models import User
+from business.models import Product
 
-# Create your models here.
+
+class PurBeurreUser(models.Model):
+    """
+    extend user model for add favoris product
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favoris_product = models.ManyToManyField(Product, related_name="user")
