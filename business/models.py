a57a347bc129ea48682d0a2b9f7e75adf3dddb3e
youpiyoful <yoanfornari@gmail.com>
2020-07-19 05:16:26 +0200

    create product and category models

--- business/models.py
+++ business/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from user.models import PurBeurreUser
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Product of categories : name, nutriscore, image...
+    """
+    product_name = models.CharField()
+    nutriscore = models.CharField()
+    image = models.ImageField()
+    categories = models.ManyToManyField(Category, related_name='products')
+    user = models.ManyToManyField(
+        PurBeurreUser,
+        related_name='favoris_product'
+    )
+    fat = models.IntegerField()
+    saturated_fat = models.IntegerField()
+    sugars = models.IntegerField()
+    salt = models.IntegerField()
+    openfoodfacts_link = models.CharField()
+    # TODO dans openfoodfacts on trouve ces informations à ce chemin
+    # nutriments.get('salt'), nutriments.get('sugars') etc...
+
+
+class Category(models.Model):
+    """
+    Category of product : name
+    """
+    category_name = models.CharField()
+    # bind_category = models.ManyToManyField("self")
+    products = models.ManyToManyField(Product, related_name='categories')
