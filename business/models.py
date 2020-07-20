decc5f86c1e468a5bee4a15a22980d7bae933f96
youpiyoful <yoanfornari@gmail.com>
2020-07-20 14:49:28 +0200

    :wastebasket: i clean the wrong code

--- business/models.py
+++ business/models.py
@@ -1,24 +1,18 @@
 from django.db import models
-from user.models import PurBeurreUser
 
 
 class Product(models.Model):
     """
     Product of categories : name, nutriscore, image...
     """
-    product_name = models.CharField()
-    nutriscore = models.CharField()
-    image = models.ImageField()
-    categories = models.ManyToManyField(Category, related_name='products')
-    user = models.ManyToManyField(
-        PurBeurreUser,
-        related_name='favoris_product'
-    )
+    product_name = models.CharField(max_length=100)
+    nutriscore = models.CharField(max_length=100)
+    image = models.CharField(max_length=150)
     fat = models.IntegerField()
     saturated_fat = models.IntegerField()
     sugars = models.IntegerField()
     salt = models.IntegerField()
-    openfoodfacts_link = models.CharField()
+    openfoodfacts_link = models.CharField(max_length=100)
     # TODO dans openfoodfacts on trouve ces informations à ce chemin
     # nutriments.get('salt'), nutriments.get('sugars') etc...
 
@@ -27,6 +21,5 @@ class Category(models.Model):
     """
     Category of product : name
     """
-    category_name = models.CharField()
-    # bind_category = models.ManyToManyField("self")
+    category_name = models.CharField(max_length=100)
     products = models.ManyToManyField(Product, related_name='categories')
