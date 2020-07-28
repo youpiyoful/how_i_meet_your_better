41181d31c092abcf647c9fd7af7f72847e480ecc
youpiyoful <yoanfornari@gmail.com>
2020-07-28 06:24:01 +0200

    add omit file

--- coverage_script.py
+++ coverage_script.py
@@ -1,4 +1,7 @@
 import os
 
-os.system("coverage run --source='.' --omit='venv/*','user/migrations/*'  manage.py test")
+os.system("coverage run --source='user' --omit='user/__init__.py','user/admin.py','user/tests.py','user/apps.py','user/urls.py','user/migrations/*'  manage.py test user")
+os.system("coverage report")
+
+os.system("coverage run --source='business' --omit='business/__init__.py','business/admin.py','business/tests.py','business/apps.py','business/urls.py','business/migrations/*'  manage.py test business")
 os.system("coverage report")
