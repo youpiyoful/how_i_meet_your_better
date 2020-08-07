63c441e0bceeb6ec666ac0c1ce071b3430001919
youpiyoful <yoanfornari@gmail.com>
2020-08-07 05:47:16 +0200

    :construction: add a new file to omit

--- coverage_script.py
+++ coverage_script.py
@@ -3,5 +3,5 @@ import os
 os.system("coverage run --source='user' --omit='user/__init__.py','user/admin.py','user/tests*','user/apps.py','user/urls.py','user/migrations/*'  manage.py test user")
 os.system("coverage report")
 
-os.system("coverage run --source='business' --omit='business/__init__.py','business/admin.py','business/tests*','business/apps.py','business/urls.py','business/migrations/*'  manage.py test business")
+os.system("coverage run --source='business' --omit='business/__init__.py','business/admin.py','business/tests*','business/launch_data_fill.py','business/apps.py','business/urls.py','business/migrations/*'  manage.py test business")
 os.system("coverage report")
