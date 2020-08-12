b68d439f3bf67cb2d7b864d0b54436504e197ce7
youpiyoful <yoanfornari@gmail.com>
2020-08-12 18:37:21 +0200

    :sparkles: create a form for the search input

---
+++ business/form.py
@@ -0,0 +1,18 @@
+from django import forms
+
+
+class SearchFoodForm(forms.Form):
+    """
+    model form for research of substitute from a food_name
+    """
+    product_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'type': 'text',
+                'placeholder': 'Produit',
+                'class': 'form-control js-scroll-trigge',
+                'id': 'product_name',
+                'aria-describedby': 'basic-addon3',
+            }
+        )
+    )
