553e2f3526430a03d95114fa947aacd7a05834ef
youpiyoful <yoanfornari@gmail.com>
2020-08-21 00:59:49 +0200

    :white_check_mark: work on register test
    i test the register functionality with a fake profile

--- user/test_functional.py
+++ user/test_functional.py
@@ -2,7 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 
 
-class MySeleniumTests(StaticLiveServerTestCase):
+class MyUserSeleniumTests(StaticLiveServerTestCase):
     fixtures = ['user-data.json']
 
     @classmethod
@@ -17,10 +17,26 @@ class MySeleniumTests(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def test_login(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        self.selenium.get('%s%s' % (self.live_server_url, 'my-account/login'))
         username_input = self.selenium.find_element_by_name("username")
 
         username_input.send_keys('yoanfornari@gmail.com')
         password_input = self.selenium.find_element_by_name("password")
         password_input.send_keys('123')
         self.selenium.find_element_by_xpath('//input[@value="Connexion"]').click()
+
+    def test_create_account(self):
+        self.selenium.get('%s%s' % (self.live_server_url, 'my-account/register'))
+        firstname_input = self.selenium.find_element_by_id("firstname")
+        lastname_input = self.selenium.find_element_by_id("lastname")
+        email_input = self.selenium.find_element_by_id('inputEmail1')
+        password_input = self.selenium.find_element_by_id('inputPassword1')
+        password_confirmation_input = self.selenium.find_element_by_id('inputPassword2')
+        firstname_input.send_keys('Gabin')
+        lastname_input.send_keys('Fornari')
+        email_input.send_keys('gabin@gmail.com')
+        password_input.send_keys('123')
+        password_confirmation_input.send_keys('123')
+        self.selenium.find_element_by_xpath('//input[@value="Inscription"]').click()
+    
+
