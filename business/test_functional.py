43eb75c23afc01c88fa9aeaaa61cffc72cdef352
youpiyoful <yoanfornari@gmail.com>
2020-08-21 15:36:20 +0200

    :white_check_mark: add test in commentary

--- business/test_functional.py
+++ business/test_functional.py
@@ -22,3 +22,46 @@ class MyBusinessSeleniumTests(StaticLiveServerTestCase):
         input_search = self.selenium.find_element_by_id('product_name')
         input_search.send_keys('Acras de morue')
         self.selenium.find_element_by_xpath('//button[@value="Chercher"]').click()
+
+
+
+# from time import sleep
+# #import tempfile
+
+# from selenium import webdriver
+# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+# from selenium.webdriver import DesiredCapabilities
+# from selenium.webdriver import FirefoxOptions
+# from selenium.webdriver import FirefoxProfile
+
+# capabilities = DesiredCapabilities.FIREFOX.copy()
+# capabilities['browserName'] = 'firefox'
+# capabilities['acceptInsecureCerts'] = False
+# capabilities['marionette'] = True
+
+# binary = FirefoxBinary('/mnt/c/Program Files/Mozilla Firefox/firefox.exe')
+# profile = FirefoxProfile('/mnt/c/Users/yoanf/AppData/Roaming/Mozilla/Firefox/Profiles/1tz7ljhm.default')
+# options = FirefoxOptions()
+
+
+# # profile = tempfile.mkdtemp('.selenium')
+# # print("*** Using profile: {}".format(profile))
+# def run_test():
+#     # options.headless = True
+#     options.log.level = "trace"
+#     options.profile = profile
+#     options.binary = binary
+
+#     driver = webdriver.Firefox(
+#         options=options,
+#         capabilities=capabilities,
+#         executable_path="/mnt/c/WebDriver/bin/geckodriver.exe",)
+#         service_args=["--marionette-port", "2828"])
+
+#     driver.get('www.selenium.com')
+#     sleep(5)
+#     driver.close()
+
+
+# if __name__ == "__main__":
+#     run_test()
