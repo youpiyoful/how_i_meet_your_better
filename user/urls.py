f6e114a07be75da0facfd397ab6abebd264daccd
youpiyoful <yoanfornari@gmail.com>
2020-08-22 04:17:51 +0200

    :sparkles: create a new path  for display_favorite

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,6 @@ app_name = 'user'
 urlpatterns = [
     path("account/", views.my_account, name="my_account"),
     path("login", views.authentication, name="login"),
-    # path("loger", views.loger, name="loger"),
     path("logout", views.logout_view, name="logout"),
     path(
             "register",
@@ -18,4 +17,5 @@ urlpatterns = [
         "record_favorite",
         views.record_favorite_substitute,
         name="record_favorite"),
+    path("favorite", views.display_favorite_food, name="favorite_food")
 ]
