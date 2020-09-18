d469f5f1836c4dfc7ee1f742feb8cfb061ac8d46
youpiyoful <yoanfornari@gmail.com>
2020-09-18 12:45:02 +0200

    :bug: fix a pb with fixtures
    comment the line about fixtures

--- business/test_functional.py
+++ business/test_functional.py
@@ -4,7 +4,7 @@ from django.shortcuts import reverse
 
 
 class MyBusinessSeleniumTests(StaticLiveServerTestCase):
-    fixtures = ['user-data.json']
+    # fixtures = ['user-data.json']
 
     @classmethod
     def setUpClass(cls):
@@ -22,3 +22,18 @@ class MyBusinessSeleniumTests(StaticLiveServerTestCase):
         input_search = self.selenium.find_element_by_id('product_name')
         input_search.send_keys('Acras de morue')
         self.selenium.find_element_by_xpath('//button[@value="Chercher"]').click()
+
+
+######################################
+# from selenium import webdriver
+# import time
+
+# browser = webdriver.Firefox()
+# time.sleep(10)
+# browser.get('http://www.univ-orleans.fr')
+# assert 'Université' in browser.title
+
+# elem = browser.find_element_by_id('banniere')
+# assert(elem is not None)
+
+# browser.quit()
