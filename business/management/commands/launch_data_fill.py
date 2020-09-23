6b9708a2a520cb8ad3f4a0f77f893cbb421347a0
youpiyoful <yoanfornari@gmail.com>
2020-09-23 17:03:42 +0200

    :wrench: change limit for cat in launch data fill

--- business/management/commands/launch_data_fill.py
+++ business/management/commands/launch_data_fill.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
         # retrieve category
         off.retrieve_all_category_name_from_open_food_facts_api()
         # retrieve category from our db
-        list_of_cat = Category.objects.all()[:300]
+        list_of_cat = Category.objects.all()[:200]
         # call method retrieve product with each cat in list_of_cat
         for cat in list_of_cat:
             off.retrieve_food_with_url_category(cat)
