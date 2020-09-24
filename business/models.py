fa775bc7036790d60b453330e499da10a6c707a9
youpiyoful <yoanfornari@gmail.com>
2020-09-25 01:20:57 +0200

    :recycle: best practice
    i use black for refactor my code

--- business/models.py
+++ business/models.py
@@ -5,50 +5,47 @@ class Product(models.Model):
     """
     Product of categories : name, nutriscore, image...
     """
+
     product_name = models.CharField(max_length=300, unique=True)
     nutriscore = models.CharField(max_length=1, null=False)
-    image_url = models.URLField(
-        verbose_name="Url de l'image du produit",
-        null=True)
+    image_url = models.URLField(verbose_name="Url de l'image du produit", null=True)
     product_url = models.URLField(
         verbose_name="Url du produit",
         unique=True,
         null=False,
-        default="aucune url trouvé")
+        default="aucune url trouvé",
+    )
     fat = models.DecimalField(max_digits=12, decimal_places=8)
     saturated_fat = models.DecimalField(max_digits=12, decimal_places=8)
     sugars = models.DecimalField(max_digits=12, decimal_places=8)
     salt = models.DecimalField(max_digits=12, decimal_places=8)
     # openfoodfacts_link = models.CharField(max_length=100)
-    # TODO dans openfoodfacts on trouve ces informations à ce chemin
-    # nutriments.get('salt'), nutriments.get('sugars') etc...
 
     def __str__(self):
         return self.product_name
 
     class Meta:
         verbose_name = "Produit"
-        ordering = ['product_name']
+        ordering = ["product_name"]
 
 
 class Category(models.Model):
     """
     Category of product : name, url_category
     """
+
     category_name = models.CharField(max_length=300)
-    products = models.ManyToManyField(Product, through='CategoriesProducts')
+    products = models.ManyToManyField(Product, through="CategoriesProducts")
     url_category = models.URLField(
-        verbose_name="Url de la categorie",
-        unique=True,
-        null=False,
-        max_length=600)
+        verbose_name="Url de la categorie", unique=True, null=False, max_length=600
+    )
 
     def __str__(self):
         return self.category_name
 
     class Meta:
         verbose_name = "Catégorie"
-        ordering = ['category_name']
+        ordering = ["category_name"]
 
 
 class CategoriesProducts(models.Model):
@@ -58,6 +55,7 @@ class CategoriesProducts(models.Model):
     The lower the score, the more general the category
     and vice versa.
     """
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     hyerarchie_score = models.IntegerField(default=0)
