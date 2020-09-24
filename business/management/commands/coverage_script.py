8887e1b562ea419df6c5ada757061854e1608bb2
youpiyoful <yoanfornari@gmail.com>
2020-09-24 16:13:27 +0200

    :white_check_mark: add the user and business functional test to the ommit files

--- business/management/commands/coverage_script.py
+++ business/management/commands/coverage_script.py
@@ -5,8 +5,8 @@ class Command(BaseCommand):
     help = "launch all the test with coverage print"
 
     def handle(self, *args, **otions):
-        os.system("coverage run --source='user' --omit='user/__init__.py','user/admin.py','user/tests*','user/apps.py','user/urls.py','user/migrations/*'  manage.py test user.tests")
+        os.system("coverage run --source='user' --omit='user/__init__.py','user/admin.py','user/tests*','user/apps.py','user/urls.py','user/migrations/*','user/test_functional.py'  manage.py test user.tests")
         os.system("coverage report")
 
-        os.system("coverage run --source='business' --omit='business/__init__.py','business/admin.py','business/tests*','business/launch_data_fill.py','business/apps.py','business/urls.py','business/migrations/*'  manage.py test business.tests business.tests_unit")
+        os.system("coverage run --source='business' --omit='business/__init__.py','business/admin.py','business/tests*','business/launch_data_fill.py','business/apps.py','business/urls.py','business/migrations/*','business/test_functional.py'  manage.py test business.tests business.tests_unit")
         os.system("coverage report")
