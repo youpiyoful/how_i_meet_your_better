5a4d1a6ca01740dd5955c5d70ba17741f29e3c99
youpiyoful <yoanfornari@gmail.com>
2020-09-25 01:23:54 +0200

    :bug: fix pb with input submit button

--- user/test_functional.py
+++ user/test_functional.py
@@ -21,7 +21,7 @@ class MyUserSeleniumTests(StaticLiveServerTestCase):
         self.selenium.get('%s%s' % (self.live_server_url, '/my-account/login'))
         username_input = self.selenium.find_element_by_name("email")
 
-        username_input.send_keys('gabinfornari@gmail.com')
+        username_input.send_keys('gabin@gmail.com')
         password_input = self.selenium.find_element_by_name("password_field")
         password_input.send_keys('123')
         # self.selenium.find_element_by_xpath('//input[@value="Connexion"]').click()
@@ -29,18 +29,18 @@ class MyUserSeleniumTests(StaticLiveServerTestCase):
         submit_btn.click()
 
 
-    # def test_create_account(self):
-    #     self.selenium.get('%s%s' % (self.live_server_url, 'my-account/register'))
-    #     firstname_input = self.selenium.find_element_by_id("firstname")
-    #     lastname_input = self.selenium.find_element_by_id("lastname")
-    #     email_input = self.selenium.find_element_by_id('inputEmail1')
-    #     password_input = self.selenium.find_element_by_id('inputPassword1')
-    #     password_confirmation_input = self.selenium.find_element_by_id('inputPassword2')
-    #     firstname_input.send_keys('Yoan')
-    #     lastname_input.send_keys('Fornari')
-    #     email_input.send_keys('yoan@gmail.com')
-    #     password_input.send_keys('123')
-    #     password_confirmation_input.send_keys('123')
-    #     self.selenium.find_element_by_xpath('//input[@value="Inscription"]').click()
+    def test_create_account(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/my-account/register'))
+        firstname_input = self.selenium.find_element_by_id("firstname")
+        lastname_input = self.selenium.find_element_by_id("lastname")
+        email_input = self.selenium.find_element_by_id('inputEmail1')
+        password_input = self.selenium.find_element_by_id('inputPassword1')
+        password_confirmation_input = self.selenium.find_element_by_id('inputPassword2')
+        firstname_input.send_keys('Yoan')
+        lastname_input.send_keys('Fornari')
+        email_input.send_keys('yoan@gmail.com')
+        password_input.send_keys('123')
+        password_confirmation_input.send_keys('123')
+        self.selenium.find_element_by_id('submit-form').click()
     
 
