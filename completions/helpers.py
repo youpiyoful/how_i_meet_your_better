e498dcef4e4b7d448bd0653f06a6d3c08cf309e4
youpiyoful <yoanfornari@gmail.com>
2020-10-09 23:56:45 +0200

    :hammer: add a limit of 15 for the list of autocompletion

--- completions/helpers.py
+++ completions/helpers.py
@@ -12,16 +12,16 @@ def get_completions(term):
     order = getattr(settings, "COMPLETIONS_ORDER", ['?'])
     if not isinstance(order, list):
         raise ImproperlyConfigured(
-            f"COMPLETIONS_ORDER must be a list of strings."
+            f"{order} must be a list of strings."
         )
     method = getattr(settings, "COMPLETIONS_METHOD", 'icontains')
     if not isinstance(method, str):
-        raise ImproperlyConfigured(f"COMPLETIONS_ORDER must be a string.")
+        raise ImproperlyConfigured(f"{order} must be a string.")
     kwargs = {f"{field}__{method}": term}
     return list(
         model.objects.filter(**kwargs)
         .order_by(*order)
-        .values_list(field, flat=True)
+        .values_list(field, flat=True)[:15]
     )
 
 
