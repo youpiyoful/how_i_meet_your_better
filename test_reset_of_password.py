f811327c12b25f4ad2929276a488a22a66e823f2
youpiyoful <yoanfornari@gmail.com>
2020-10-09 18:37:54 +0200

    :white_check_mark: clean the test
    delete create user from setupClass and create a new method setup for create user

--- test_reset_of_password.py
+++ test_reset_of_password.py
@@ -2,35 +2,28 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 from django.contrib.auth.models import User
 from django.core import mail
+from django.shortcuts import reverse
 
 
 class MySeleniumTests(StaticLiveServerTestCase):
     # fixtures = ['user-data.json']
 
-    # def setUp(self):
-    #     """
-    #     init the db
-    #     """
-    #     self.register = {
-    #         "first_name": "gabin",
-    #         "last_name": "fornari",
-    #         "username": "gabin@gmail.com",
-    #         "email": "gabin@gmail.com",
-    #         "password": "123",
-    #     }
-    #     User.objects.create_user(**self.register)
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.register = {
+    def setUp(self):
+        """
+        init the db
+        """
+        self.register = {
             "first_name": "gabin",
             "last_name": "fornari",
             "username": "gabin@gmail.com",
             "email": "gabin@gmail.com",
             "password": "123",
         }
-        User.objects.create_user(**cls.register)
+        User.objects.create_user(**self.register)
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
 
@@ -39,7 +32,8 @@ class MySeleniumTests(StaticLiveServerTestCase):
         this method checks that a user
         can reset their password
         """
-        self.selenium.get("%s%s" % (self.live_server_url, "/my-account/login"))
+        url = reverse('user:login')
+        self.selenium.get(self.live_server_url + url)
         link_to_password_reset = self.selenium.find_element_by_class_name('reset-password')
         link_to_password_reset.click()
         email_input = self.selenium.find_element_by_name('email')
