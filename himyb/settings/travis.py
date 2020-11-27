540ef69a5aa297ab9554d0b7114c56f955e9970e
youpiyoful <yoanfornari@gmail.com>
2020-11-27 02:39:26 +0100

    :wrench: change db connection

--- himyb/settings/travis.py
+++ himyb/settings/travis.py
@@ -3,10 +3,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'himyb',
-        'USER': 'youpiyoful',
-        'PASSWORD': 12345,
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
