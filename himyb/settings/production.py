f5395935f0323db02f7f918516871ee057c1116f
youpiyoful <yoanfornari@gmail.com>
2020-11-28 02:51:25 +0100

    :heavy_plus_sign: add sentry to the setting production

--- himyb/settings/production.py
+++ himyb/settings/production.py
@@ -1,5 +1,17 @@
 from .base import *
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://f9cdebed656544dba8fdec428470c116@o483455.ingest.sentry.io/5535646",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 # import os
 
 # DEBUG = False
